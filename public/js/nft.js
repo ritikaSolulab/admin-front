58e5cb7822339016d992a30a0b4bdae20bb9099e
ritikaSolulab <ritika.s@solulab.com>
2023-06-16 14:11:30 +0530

    changes in cms,royalties,nft,super-admin | in progress

--- public/js/nft.js
+++ public/js/nft.js
@@ -14,73 +14,8 @@ alert = function() {
 };
 
 
-const loadProviders = () => {
-    // providersTable = $('#transactionTable').DataTable({
-    //     // serverSide: true,
-    //     searching: true,
-    //     ajax: {
-    //         url: `${providerApi}/approvedList`,
-    //         data : (data) => {
-    //             const value = $('#dateFilter').val();
-    //             if (value.split('-').length) {
-    //                 let query = '';
-    //                 query+=`startDate=${value.split('-')[0]}`;
-    //                 query+=`&endDate=${value.split('-')[1]}`;
-    //                 return query;
-    //             }
-    //         }
-    //     },
-    //     columns: [{
-    //         data: "firstName"
-    //     },
-    //     {
-    //         mRender(data, type, row, meta) {
-    //             console.log('type', type);
-    //             return type == 'sort' ?  (new Date(row.createdTime)).getTime() : row.createdTime;
-    //         }
-    //     },
-    //     {
-    //         data: "document.organization_name"
-    //     },
-    //     {
-    //         mRender(data, type, row, meta) {
-    //             if (row.isActive && !row.isRejected) {
-    //                 return 'Active';
-    //             } else {
-    //                 return 'De-active';
-    //             }
-    //         }
-    //     },
-    //     {
-    //         data : 'plan'
-    //     },
-    //     {
-    //         mRender(data, type, row) {
-    //             return `<a href="/admin/provider_details/${row._id}"><i class="fa fa-eye"></i></a>`;
-
-    //         },
-    //         orderable: false,
-    //         searchable: false,
-    //     }
-    //     ]
-    // }
-    // );
-    // $('#providersTable_filter input').unbind();
-    // $('#providersTable_filter input').bind('keyup', function (e) {
-    //     console.log('here');
-    //     console.log(this.value);
-    //     if (e.keyCode == 13) {
-    //         providersTable.search(this.value).draw();
-    //     }
-    // });
-    
-}
-
-
-const loadPendingProviders = () => {
-    transactionTable = $('#transactionTable').DataTable({
-        // serverSide: true,
-        // searching: false,
+const nft = () => {
+    nftTable = $('#nftTable').DataTable({
         columns: [
             { data: 'srno' },
             { data: 'nft_name' },
@@ -112,63 +47,13 @@ const loadPendingProviders = () => {
             },
 
         ]
-        // ajax: {
-        //     url: providePendingApi,
-        //     data : (data) => {
-        //         const value = $('#dateFilter').val();
-        //         if (value.split('-').length) {
-        //            let query = '';
-        //            query+=`startDate=${value.split('-')[0]}`;
-        //            query+=`&endDate=${value.split('-')[1]}`;
-        //             return query;
-        //         }
-        //     }
-        // },
-        // columns: [{
-        //     mRender(data, type, row) {
-        //         if(row.document)
-        //         {
-        //             return row.document.duns_number;
-        //         }else{
-        //             return '';
-        //         }
-
-        //     }
-        // },
-        // {
-        //     data: "firstName"
-        // },
-        // {
-        //     data: "createdTime"
-        // },
-        // {
-        //     mRender(data, type, row) {
-        //         if(row.document)
-        //         {
-        //             return row.document.organization_name;
-        //         }else{
-        //             return '';
-        //         }
-
-        //     }
-        // },
-        // {
-        //     mRender(data, type, row) {
-        //         return `<a href="/admin/provider_pending_approvals/${row._id}"><i class="fa fa-eye"></i></a>`;
-
-        //     },
-        //     orderable: false,
-        //     searchable: false,
-        // }
-        // ]
     });
 
 }
 
 $(document).ready(function () {
 
-    loadProviders();
-    loadPendingProviders();
+    nft();
 });
 
 const dateFilter = (value) => {
--- public/js/transaction.js
+++ public/js/transaction.js
@@ -13,74 +13,8 @@ alert = function() {
   console.log(new Error().stack);
 };
 
-
-const loadProviders = () => {
-    // providersTable = $('#transactionTable').DataTable({
-    //     // serverSide: true,
-    //     searching: true,
-    //     ajax: {
-    //         url: `${providerApi}/approvedList`,
-    //         data : (data) => {
-    //             const value = $('#dateFilter').val();
-    //             if (value.split('-').length) {
-    //                 let query = '';
-    //                 query+=`startDate=${value.split('-')[0]}`;
-    //                 query+=`&endDate=${value.split('-')[1]}`;
-    //                 return query;
-    //             }
-    //         }
-    //     },
-    //     columns: [{
-    //         data: "firstName"
-    //     },
-    //     {
-    //         mRender(data, type, row, meta) {
-    //             console.log('type', type);
-    //             return type == 'sort' ?  (new Date(row.createdTime)).getTime() : row.createdTime;
-    //         }
-    //     },
-    //     {
-    //         data: "document.organization_name"
-    //     },
-    //     {
-    //         mRender(data, type, row, meta) {
-    //             if (row.isActive && !row.isRejected) {
-    //                 return 'Active';
-    //             } else {
-    //                 return 'De-active';
-    //             }
-    //         }
-    //     },
-    //     {
-    //         data : 'plan'
-    //     },
-    //     {
-    //         mRender(data, type, row) {
-    //             return `<a href="/admin/provider_details/${row._id}"><i class="fa fa-eye"></i></a>`;
-
-    //         },
-    //         orderable: false,
-    //         searchable: false,
-    //     }
-    //     ]
-    // }
-    // );
-    // $('#providersTable_filter input').unbind();
-    // $('#providersTable_filter input').bind('keyup', function (e) {
-    //     console.log('here');
-    //     console.log(this.value);
-    //     if (e.keyCode == 13) {
-    //         providersTable.search(this.value).draw();
-    //     }
-    // });
-    
-}
-
-
-const loadPendingProviders = () => {
+const transaction = () => {
     transactionTable = $('#transactionTable').DataTable({
-        // serverSide: true,
-        // searching: false,
         columns: [
             { data: 'srno' },
             { data: 'txn' },
@@ -142,63 +76,13 @@ const loadPendingProviders = () => {
             }
 
         ]
-        // ajax: {
-        //     url: providePendingApi,
-        //     data : (data) => {
-        //         const value = $('#dateFilter').val();
-        //         if (value.split('-').length) {
-        //            let query = '';
-        //            query+=`startDate=${value.split('-')[0]}`;
-        //            query+=`&endDate=${value.split('-')[1]}`;
-        //             return query;
-        //         }
-        //     }
-        // },
-        // columns: [{
-        //     mRender(data, type, row) {
-        //         if(row.document)
-        //         {
-        //             return row.document.duns_number;
-        //         }else{
-        //             return '';
-        //         }
-
-        //     }
-        // },
-        // {
-        //     data: "firstName"
-        // },
-        // {
-        //     data: "createdTime"
-        // },
-        // {
-        //     mRender(data, type, row) {
-        //         if(row.document)
-        //         {
-        //             return row.document.organization_name;
-        //         }else{
-        //             return '';
-        //         }
-
-        //     }
-        // },
-        // {
-        //     mRender(data, type, row) {
-        //         return `<a href="/admin/provider_pending_approvals/${row._id}"><i class="fa fa-eye"></i></a>`;
-
-        //     },
-        //     orderable: false,
-        //     searchable: false,
-        // }
-        // ]
     });
 
 }
 
 $(document).ready(function () {
 
-    loadProviders();
-    loadPendingProviders();
+    transaction();
 });
 
 const dateFilter = (value) => {
