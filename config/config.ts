73d43a139b1974243d402de3c7eea778b56c1080
ritikaSolulab <126669605+ritikaSolulab@users.noreply.github.com>
2023-06-27 18:15:09 +0530

    change in config.ts

--- config/config.ts
+++ config/config.ts
@@ -3,7 +3,7 @@ class config {
   host!: string;
 
   constructor() {
-    this.port = Number(process.env.PORT) || 5000;
+    this.port = Number(process.env.PORT) || 3000;
     this.host = process.env.HOST || 'localhost';
   }
 
