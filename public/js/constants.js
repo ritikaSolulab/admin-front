d89e47d3f81cad3447243904fdcf870961c19eea
adil-solulab <adil101078@gmail.com>
2023-07-10 12:10:57 +0530

    merge development-pr into staging
    Signed-off-by: adil-solulab <adil101078@gmail.com>

--- public/js/constants.js
+++ public/js/constants.js
@@ -1,6 +1,6 @@
 const config = {
-    // SERVER_URL: 'http://50.19.128.91/',
-    SERVER_URL: 'http://localhost:3000/',
+    SERVER_URL: 'http://50.19.128.91/',
+    // SERVER_URL: 'http://localhost:3000/',
     URLS: {
         LOGIN_URL: 'admin/login',
         FORGOT_PASS: 'admin/forgot-password',
